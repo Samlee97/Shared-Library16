ad37b6d97541a7f30811a17769c78153ed39c0e0
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-17 15:15:36 +0530

    Create createQABranchTesting.groovy

---
+++ vars/createQABranchTesting.groovy
@@ -0,0 +1,21 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createDevBranch(String data, String URL){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def branchName = '"'+resultJson.branch2+'"'
+def projUrl = '"'+resultJson.url+'"'
+  
+  httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": ${branchName},
+    "startPoint": "refs/heads/master"
+}""", responseHandle: 'NONE', url: "http://" + "${URL}" + "/rest/api/1.0/projects/DEM/repos/web_1/branches"
+}
+
+
+def call(String BITBUCKETURL){
+ def request = libraryResource 'data.json'
+ createDevBranch(request,BITBUCKETURL)
+}

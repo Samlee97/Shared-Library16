02ea66ac4408fcce8579ea0c404842ed51c96046
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-17 15:28:42 +0530

    Delete createRepo.groovy

--- vars/createRepo.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
-    "name": "test_123",
-    "scmId": "git",
-    "forkable": true
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/DEM/repos'
-}

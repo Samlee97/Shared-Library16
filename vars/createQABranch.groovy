8290b3154da8e20c42be3884fa0bc3df158d990e
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-17 15:28:08 +0530

    Delete createQABranch.groovy

--- vars/createQABranch.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
-    "name": "QA",
-    "startPoint": "refs/heads/master"
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/TEST_12/repos/test_123/branches'
-
-}

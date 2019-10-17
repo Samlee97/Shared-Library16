eb56d91b9709ff88a231c77bfe4f18a5c9f94d24
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-17 12:30:56 +0530

    Create createDevBranch.groovy

---
+++ vars/createDevBranch.groovy
@@ -0,0 +1,7 @@
+def call(){
+httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
+    "name": "DEV",
+    "startPoint": "refs/heads/master"
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/TEST_12/repos/test_123/branches'
+
+}

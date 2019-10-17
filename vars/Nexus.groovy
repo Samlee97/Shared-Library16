3a28362e976d0a0d6fa7c8ed4a221ce74a2b939b
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-17 19:38:33 +0530

    Update Nexus.groovy

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -4,19 +4,8 @@ import groovy.json.JsonSlurper
 Nexus(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-                    def artifactId:"Web_Build_Artifacts",
-                    def classifier: "",
-                    def filePath: "target/JenkinsWar.war",
-                    def type: "war",
-                    def credentialsId: "nexus",
-                    def groupId: "maven-group",
-                    def nexusUrl: "ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus",
-                    def nexusVersion: "nexus",
-                    def protocol: "http",
-                    def repository: "kishan-test",
-                    def version: "${env.BUILD_NUMBER}"
-
-
+                   
+def artf='"'+resultJson.artifactId+'""'
 
                       }
 def call(){

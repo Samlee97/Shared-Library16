1a1da39bd811f18952ffc2e732857f777834ca18
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-18 17:09:13 +0530

    Update deployToEC2.groovy

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,3 +1,6 @@
+import groovy.json.JsonSlurper
+@NonCPS
+
 def playbook(String inventory, String rolePath) {
     echo "Execute Ansible role..."
     def copyArtifact = "cp /var/lib/jenkins/workspace/JenkinsSharedLibrary/target/*.war /var/lib/jenkins/workspace/JenkinsSharedLibrary/ansiblerole/roles/DeployFromNexus/files"
@@ -5,16 +8,17 @@ def playbook(String inventory, String rolePath) {
     echo "Command is: ${shellCommand}"
     sh(shellCommand)
 }
-def gitClone(String data) {
+def gitClone() {
    echo "Executing Git Clone to download Artifact from Nexus to EC2 ansible role"
-
+   
    def removeAnsibleRole = " rm -rf ansiblerole"
    sh(removeAnsibleRole)
-    
+   
+   def request = libraryResource 'data.json'
    def jsonSlurper = new JsonSlurper() 
-   def resultJson = jsonSlurper.parseText(data)
-   url = ansible.url[0]
-    println('url')
+   def resultJson = jsonSlurper.parseText(request)
+   def url = ansible.url[0]
+   println('url')
  
    def shellCommand = "git clone ${url}"
    echo "Command is: ${shellCommand}"

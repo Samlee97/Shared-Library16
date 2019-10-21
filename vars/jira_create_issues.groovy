9382669852a160aa9b31f422c218ebf4d56792dc
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-21 15:44:55 +0530

    Update jira_create_issues.groovy

--- vars/jira_create_issues.groovy
+++ vars/jira_create_issues.groovy
@@ -3,11 +3,11 @@ import groovy.json.JsonSlurper
 createIssues(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-def projectName = '"'+resultJson.name+'"'
+def projectName = '"'+resultJson.jira.name+'"'
 def length = 3
-def projLength = resultJson.name.size()
+def projLength = resultJson.jira.name.size()
  if(projLength>=3){
-  key=resultJson.name.substring(0, Math.min(projLength, length)).toUpperCase();
+  key=resultJson.jira.name.substring(0, Math.min(projLength, length)).toUpperCase();
  }
  else {
    def appendStr = "";
@@ -15,9 +15,9 @@ def projLength = resultJson.name.size()
    int len = currentLength%projLength;
    int repeat = currentLength/projLength;
    for (int i=0;i<repeat;i++) {
-    appendStr = appendStr + resultJson.name;
+    appendStr = appendStr + resultJson.jira.name;
    }
-   appendStr=appendStr+resultJson.name.substring(0, Math.min(projectName.length(), len));
+   appendStr=appendStr+resultJson.jira.name.substring(0, Math.min(projectName.length(), len));
    key=appendStr.toUpperCase();
  }
  def projKey = '"'+key+'"'

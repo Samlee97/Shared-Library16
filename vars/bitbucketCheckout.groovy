edcc24097812b5d24c35db9055b0d6177506eb4e
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2019-10-22 13:37:02 +0530

    Update bitbucketCheckout.groovy

--- vars/bitbucketCheckout.groovy
+++ vars/bitbucketCheckout.groovy
@@ -1,4 +1,5 @@
 def call(){
-    def checkout = "git clone http://rig@18.224.68.30:7990/scm/dem/app.git"
+    def checkout = "git clone http:///rig:${rig_password}@18.224.68.30:7990/scm/dem/app.git"
+    //git clone http://rig:${rig_password}@${BB_URL}/scm/DEM/app.git
     sh(checkout)
 }
